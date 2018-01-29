4e4b31aad2a7cc0e1d233d76d42bec3f0af6c8ac
zy-lee <lizhuyang.vip@gmail.com>
2018-01-29 18:23:13 +0800

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+VotePic.create([{ src: '01.png' }, { src: '02.png' }, { src: '03.png' }, { src: '04.png' }, { src: '05.png' }, { src: '06.png' }, { src: '07.png' }, { src: '08.png' }, { src: '09.png' }, { src: '10.png' }, { src: '11.png' }, { src: '12.png' }, { src: '13.png' }, { src: '14.png' }, { src: '15.png' }, { src: '16.png' }, { src: '17.png' }, { src: '18.png' }, { src: '19.png' }, { src: '20.png' }, { src: '21.png' }, { src: '22.png' }, { src: '23.png' }, { src: '24.png' }, { src: '25.png' }, { src: '26.png' }, { src: '27.png' }, { src: '28.png' }, { src: '29.png' }, { src: '30.png' }, { src: '31.png' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
